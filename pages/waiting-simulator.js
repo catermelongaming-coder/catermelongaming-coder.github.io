45f11939cb5efa4cf629eb0ee91024f952e0f761
catermelongaming-coder <catermelongaming@gmail.com>
2026-03-29 21:27:49 +1300

    Update waiting-simulator.js

--- pages/waiting-simulator.js
+++ pages/waiting-simulator.js
@@ -1,7 +1,18 @@
 const timeEl = document.querySelector(".ws-time");
+const unit = document.querySelector(".ws-unit");
+
 let time = 0;
 
 setInterval(() => {
     time++;
-    timeEl.innerHTML = time;
+
+    if (time < 60) {
+        timeEl.innerHTML = time;
+        unit.innerHTML = "seconds";
+    } else {
+        let minutes = time / 60;
+        timeEl.innerHTML = minutes.toFixed(2);
+        unit.innerHTML = "minutes";
+    }
+
 }, 1000);
