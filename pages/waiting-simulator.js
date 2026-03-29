34fc0d0e89b6333128155c9b855824a12bcf268a
catermelongaming-coder <catermelongaming@gmail.com>
2026-03-29 20:24:25 +1300

    Update waiting-simulator.js

--- pages/waiting-simulator.js
+++ pages/waiting-simulator.js
@@ -1,6 +1,8 @@
 let time = document.querySelector("ws-time");
 let parsedTime = parseFloat(time.innerHTML);
 
+console.log(document.querySelector(".ws-time"));
+
 setInterval(() => {
     parsedtime += 1;
     time.innerHTML = parsedtime;
